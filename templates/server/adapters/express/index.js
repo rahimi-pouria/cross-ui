6e881646acf9fef1c9f6e4c1de4a1da1d199007c
pouriaRahimy <pouria.rahimy@gmail.com>
2025-12-26 15:48:45 +0330

    create server from node js amd create publie module

--- templates/server/adapters/express/index.js
+++ templates/server/adapters/express/index.js
@@ -1,1 +1,13 @@
-console.log('hello pouria another cross ui')
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+const port = process.env.app_PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the server!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
